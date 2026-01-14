e790a5670db0fda00cff252088baa8e5a227541b
Mawada-22 <mawadaebrahim30@gmail.com>
2026-01-15 00:04:09 +0200

    UnitOfWork

---
+++ Domain/Contarcts/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Contarcts
+{
+    public interface IUnitOfWork
+    {
+        public Task<int> SaveChangesAsync();
+
+        IGenericRepo<TKey,TEntity> GetRepo<TKey, TEntity>() where TEntity:ModelBase<TKey>;
+    }
+}
---
+++ Presistance/Repostories/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Contarcts;
+using Domain.Entities;
+using Presistance.Data;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presistance.Repostories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly StoreDbcontext _dbContext;
+        private readonly ConcurrentDictionary<string,object>Repos;
+
+        public UnitOfWork(StoreDbcontext dbcontext) 
+        {
+        _dbContext =dbcontext;
+            Repos = new();
+
+
+        }
+        public IGenericRepo<TKey, TEntity> GetRepo<TKey, TEntity>() where TEntity : ModelBase<TKey>
+        {
+            return (IGenericRepo<TKey, TEntity>) Repos.GetOrAdd(typeof(TEntity).Name, _ => new GenericRepo<TKey, TEntity>(_dbContext));
+        }
+
+        public Task<int> SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
