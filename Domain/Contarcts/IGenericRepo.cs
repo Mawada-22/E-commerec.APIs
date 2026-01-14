a85973cb962b323e57246363a73acdb764132b83
Mawada-22 <mawadaebrahim30@gmail.com>
2026-01-14 22:51:52 +0200

    GenericRepo

---
+++ Domain/Contarcts/IGenericRepo.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Contarcts
+{
+    public interface IGenericRepo<TKey,TEntity> where TEntity : ModelBase<TKey>
+    {
+        public Task<IEnumerable<TEntity>> GatAllAsync(bool asNoTracking = false);
+        public Task<TEntity>GetByIdAsync(TKey id);
+
+        public Task AddAsync(TEntity entity);
+        public void Update(TEntity entity);
+        public void Delete(TEntity entity);
+    }
+}
